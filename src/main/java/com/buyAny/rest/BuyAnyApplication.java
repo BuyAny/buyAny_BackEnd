22ded6681007eedf9fc97558982599910d7c8927
Mohamed el Ghaouth <mohamedelghaouth7991@gmail.com>
2022-03-24 23:43:58 +0100

    first commit

---
+++ src/main/java/com/buyAny/rest/BuyAnyApplication.java
@@ -0,0 +1,13 @@
+package com.buyAny.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyAnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyAnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buyAny/rest/BuyAnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyAny.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyAnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
